889be567e68f9377d42049a69a888d4ba1895bac
Tahany Alswoidy <tahanyalswoidy@gmail.com>
2020-08-04 17:36:40 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // Write your code here!
 let newHeader = document.createElement('h1');
 document.body.appendChild(newHeader);
-newHeader.Id="victory";
+newHeader.id="victory";
 newHeader.innerHTML="YOUR-NAME is the champion";
